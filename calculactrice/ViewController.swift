5cbda41697135c3d7262a2a39ed27248297bb3a0
GuillaumeSavin <guisav1996@hotmail.fr>
2021-03-01 10:12:09 +0100

    first commit

--- calculactrice/ViewController.swift
+++ calculactrice/ViewController.swift
@@ -8,12 +8,73 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var onEstAuDebuDunMot = true
+    var onADejaUneVirgule = false
+    
+    var pileDeNombres = Array<Double>()
+    var valeurAffichee : Double {
+        get {
+            return NumberFormatter().number(from: ecran.text!)!.doubleValue
+        }
+        set {
+            ecran.text = "\(newValue)"
+        }
+    }
 
+    @IBOutlet weak var ecran: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func ajoutChiffre(_ sender: UIButton) {
+        if let chiffre = sender.currentTitle {
+            if onEstAuDebuDunMot {
+                ecran.text! = chiffre
+                onEstAuDebuDunMot = false
+            } else {
+                ecran.text! += chiffre
+            }
+            
+        }
+    }
+    
+    @IBAction func calcul(_ sender: UIButton) {
+        switch sender.currentTitle {
+        case "+":
+            applique(operation: +)
+            //valeurAffichee = pileDeNombres.popLast()! + pileDeNombres.popLast()!
+        case "-":
+            applique(operation: {
+                (op1, op2) in op2 - op1
+            })
+            print("-")
+        case "×":
+            print("x")
+        case "÷":
+            print("/")
+        default:
+            print("operation non connu")
+        }
+    }
+    
+    @IBAction func enter(_ sender: Any) {
+        pileDeNombres.append(valeurAffichee)
+        onEstAuDebuDunMot = true
+        print("la pile est \(pileDeNombres)")
+    }
+    
+    func applique(operation: (Double, Double) -> Double) {
+        if pileDeNombres.count > 1 {
+            if pileDeNombres.last. {
+                <#code#>
+            }
+            valeurAffichee = operation(pileDeNombres.popLast()!, pileDeNombres.popLast()!)
+            enter(self)
+        }
+    }
+    
 
 }
 
